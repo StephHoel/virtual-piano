f9c3acfbf2096ddb91786c3ce4960458a8e29b9e
Steph Hoel <steph.hoel@gmail.com>
2023-09-28 21:50:15 -0300

    feat: add allow keys dynamically

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key")
 
+let mappedKeys = []
+
 let audio = new Audio('src/tunes/a.wav')
 
 function playTune(key) {
@@ -14,10 +16,13 @@ function playTune(key) {
 }
 
 pianoKeys.forEach((key) => {
-  console.log(key.dataset.key)
-  key.addEventListener("click", ()=>{ playTune(key.dataset.key)})
+  // console.log(key.dataset.key)
+  key.addEventListener("click", () => { playTune(key.dataset.key) })
+  
+  mappedKeys.push(key.dataset.key)
 })
 
 document.addEventListener('keydown', (e) => {
-  playTune(e.key)
+  if(mappedKeys.includes(e.key))
+    playTune(e.key)
 })
